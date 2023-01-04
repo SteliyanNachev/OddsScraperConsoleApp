a08844daedcd54491e2ca1272528cc409a209cf9
SteliyanNachev <st.g.nachev@gmail.com>
2023-01-04 07:00:24 +0200

    Fixing js pagination and getting all matches from Today section

--- ScraperConsoleApp/Program.cs
+++ ScraperConsoleApp/Program.cs
@@ -16,7 +16,6 @@
         static void Main(string[] args)
         {
             string fullUrl = "https://orbitxch.com/customer/sport/1/competition/all";
-            List<string> programmerLinks = new List<string>();
 
             var options = new ChromeOptions()
             {
@@ -24,31 +23,47 @@
             };
 
             options.AddArguments(new List<string>() { "disable-gpu" });
-            
+
             var browser = new ChromeDriver(options);
 
             browser.Navigate().GoToUrl(fullUrl);
             browser.Manage().Window.Size = new System.Drawing.Size(1800, 1009);
-            Thread.Sleep(8000);
+            //browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(8);
+            Thread.Sleep(4000);
 
-           var test = browser.PageSource;
-            //Console.WriteLine(test);
-            var commingUpEvents = browser.FindElement(By.ClassName("js-coming-up-sport-region"));
-            var matches = commingUpEvents.FindElements(By.ClassName("biab_table-wrapper"));
-            foreach (var item in matches)
+            //Finding and pressing today matches button
+            var todayMatchesButton = browser.FindElement(By.XPath("//*[@id='biab_content']/div/div[3]/div/div[1]/div[2]/div[1]/div/div[1]/span[2]/span/span/span[3]"));
+            todayMatchesButton.Click();
+            Thread.Sleep(3000);
+
+            //Getting matches information
+            var isNextButtonActive = true;
+            while (isNextButtonActive)
             {
+                var commingUpEvents = browser.FindElement(By.ClassName("js-coming-up-sport-region"));
+                var matches = commingUpEvents.FindElements(By.ClassName("biab_table-wrapper"));
 
-                var time = item.FindElement(By.ClassName("biab_market-time")).Text;
-                var teams = item.FindElement(By.ClassName("biab_market-title-team-names"));
-                var teamSpan = teams.FindElements(By.TagName("span"));
-                var team1 = teamSpan[0].Text;
-                var team2 = teamSpan[1].Text;
+                foreach (var item in matches)
+                {
 
-                Console.WriteLine($"Time: {time}  {team1} vs {team2}");
+                    var time = item.FindElement(By.ClassName("biab_market-time")).Text;
+                    var teams = item.FindElement(By.ClassName("biab_market-title-team-names"));
+                    var teamSpan = teams.FindElements(By.TagName("span"));
+                    var team1 = teamSpan[0].Text;
+                    var team2 = teamSpan[1].Text;
+
+                    Console.WriteLine($"Time: {time}  {team1} vs {team2}");
+                }
+
+                browser.FindElement(By.LinkText("Next")).Click();
+                Thread.Sleep(8000);
+
+                isNextButtonActive = browser.FindElement(By.LinkText("Next")).Enabled;
+
             }
-            browser.Quit();
-           //Console.WriteLine(table.Text);
             
-		}
+            browser.Quit();
+
+        }
     }
 }
